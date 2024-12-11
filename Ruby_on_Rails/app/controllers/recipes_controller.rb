5b4bf4d9bb49f09780ffe670398eeffe8d7ac719
Daniil Shapovalov <litdan03@gmail.com>
2024-12-12 01:28:01 +0300

    Update appearance of the app, search for recipes realized

--- Ruby_on_Rails/app/controllers/recipes_controller.rb
+++ Ruby_on_Rails/app/controllers/recipes_controller.rb
@@ -1,18 +1,27 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: [:show, :update, :destroy]
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]  # добавляем :edit
+  before_action :set_categories, only: [:new, :edit, :create, :update] # добавляем категории в действия new и edit
 
   # GET /recipes
   def index
-    @recipes = Recipe.all
     @categories = Category.all
+
+    if params[:title].present? && params[:category_id].present?
+      @recipes = Recipe.where("title ILIKE ?", "%#{params[:title]}%").where(category_id: params[:category_id])
+    elsif params[:title].present?
+      @recipes = Recipe.where("title ILIKE ?", "%#{params[:title]}%")
+    elsif params[:category_id].present?
+      @recipes = Recipe.where(category_id: params[:category_id])
+    else
+      @recipes = Recipe.all
+    end
     render :index
   end
 
   # GET /recipes/new
   def new
     @recipe = Recipe.new
-    @categories = Category.all
-    render :form # Загрузит форму show.html.erb
+    render :form
   end
 
   # GET /recipes/:id
@@ -20,11 +29,14 @@ class RecipesController < ApplicationController
     render :show
   end
 
+  # GET /recipes/:id/edit
+  def edit
+    # @recipe уже загружен через set_recipe
+  end
+
   # POST /recipes
   def create
-    @categories = Category.all
     @recipe = Recipe.new(recipe_params)
-
     if @recipe.save
       redirect_to @recipe, notice: 'Рецепт был успешно добавлен.'
     else
@@ -32,32 +44,37 @@ class RecipesController < ApplicationController
     end
   end
 
-
   # PUT /recipes/:id
   def update
     if @recipe.update(recipe_params)
-      render json: @recipe, status: :ok
+      redirect_to @recipe, notice: 'Рецепт успешно обновлен.'
     else
-      render json: @recipe.errors, status: :unprocessable_entity
+      render :edit
     end
   end
 
   # DELETE /recipes/:id
   def destroy
     @recipe.destroy
-    head :no_content
+    redirect_to recipes_path, notice: 'Рецепт был успешно удален.'
   end
 
   private
 
+  # Устанавливаем рецепт для show, edit, update и destroy
   def set_recipe
     @recipe = Recipe.find(params[:id])
   rescue ActiveRecord::RecordNotFound
-    render json: { error: 'Рецепт не найден' }, status: :not_found
+    redirect_to recipes_path, alert: 'Рецепт не найден.'
   end
 
+  # Устанавливаем список категорий для new и edit
+  def set_categories
+    @categories = Category.all
+  end
+
+  # Разрешаем параметры рецепта
   def recipe_params
     params.require(:recipe).permit(:title, :ingredients, :instructions, :preparation_time, :difficulty, :category_id, :image_url)
   end
-
 end
